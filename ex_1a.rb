5d4d38c881cdc1b578218ddb821e9a806245b33d
PadraicB <berginp@oregonstate.edu>
2022-11-21 10:56:21 -0800

    commit

---
+++ ex_1a.rb
@@ -0,0 +1,22 @@
+class Player
+    def initialize(num)
+        @name = "player " << num.to_s
+        @score = rand(10..300)
+    end
+    def display()
+        puts "Ready #@name! score:#@score"
+    end
+end
+
+players = []
+for n in 0..50 do
+players << Player.new(n)
+players[n].display()
+end
+words = [1,2,3]
+words.each do|word|
+    puts word
+end
+
+
+
